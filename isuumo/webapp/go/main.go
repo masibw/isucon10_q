d5843e1faebae1a399a4429773a08235f252ed54
harukitosa <13haruki28@gmail.com>
2020-09-12 12:51:38 +0900

    pprofの導入

--- isuumo/webapp/go/main.go
+++ isuumo/webapp/go/main.go
@@ -13,6 +13,8 @@ import (
 	"strconv"
 	"strings"
 
+	_ "net/http/pprof"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo"
@@ -239,6 +241,10 @@ func init() {
 }
 
 func main() {
+
+	go func() {
+		fmt.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 	// Echo instance
 	e := echo.New()
 	e.Debug = true
